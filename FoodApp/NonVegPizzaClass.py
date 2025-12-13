2d9909566cd1aec243a90b96a14b4daca4ace84f
Choko16 <97159421+Choko16@users.noreply.github.com>
2025-12-13 15:09:56 -0700

    Delete FoodApp directory

--- FoodApp/NonVegPizzaClass.py
+++
@@ -1,5 +0,0 @@
-from PizzaClass import Pizza
-
-class NonVegPizza(Pizza):
-    def __init__(self, price: int, calories: int, size: str, descr: str) -> None:
-        super().__init__(price, calories, size, descr)
--- FoodApp/VegBurgerClass.py
+++
@@ -1,8 +0,0 @@
-from BurgerClass import Burger
-
-class VegBurger(Burger):
-    def __init__(self, price: int, calories: int, descr: str):
-        super().__init__(price, calories, descr)
-
-    def showVegBurgerAdvert(self):
-        return f"Try our Veg Burger! {self._descr} ({self._calories} cals) only ${self._price}"
