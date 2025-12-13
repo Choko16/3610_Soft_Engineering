9eb15606fe5f57e56dc3170c115ddc2295b0b2a1
Choko16 <97159421+Choko16@users.noreply.github.com>
2025-12-13 15:10:11 -0700

    Delete ZooFactoryMethod directory

--- ZooFactoryMethod/ElephantClass.py
+++
@@ -1,8 +0,0 @@
-from IAnimalClass import IAnimal
-
-class Elephant(IAnimal):
-    def __init__(self):
-        self.__name = "Elephant"
-
-    def say(self):
-        return f"{self.__name}: Trumpet"
